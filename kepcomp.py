58f9d8be76c135b8dd5df2b9a43092341c964585
Jon Holtzman <holtz@antares.nmsu.edu>
2016-07-16 14:55:09 -0600

    updates

--- kepcomp.py
+++ kepcomp.py
@@ -11,6 +11,7 @@ import pdb
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+import astropy
 
 def kurucz_marcs(logg='LOGG_SYD_SCALING',apokasc='APOKASC_cat_v3.6.0.fits') :
     '''
@@ -40,27 +41,27 @@ def kurucz_marcs(logg='LOGG_SYD_SCALING',apokasc='APOKASC_cat_v3.6.0.fits') :
     i1,i2=match.match(l30i['APOGEE_ID'],apokasc['2MASS_ID'])
     warn=np.where(l30i['ASPCAPFLAG'][i1] & flag.aspcapflagval('ATMOS_HOLE_WARN'))[0]
     bad=np.where(l30i['ASPCAPFLAG'][i1] & flag.aspcapflagval('ATMOS_HOLE_BAD'))[0]
-    rgb=np.where(apokasc['CONS_EVSTATES'][i2] == 'RGB'))[0]
-    rc=np.where(apokasc['CONS_EVSTATES'][i2] == 'RC'))[0]
+    rgb=np.where(apokasc['CONS_EVSTATES'][i2] == 'RGB')[0]
+    rc=np.where(apokasc['CONS_EVSTATES'][i2] == 'RC')[0]
     #plt.plot(apokasc[logg][i2],l30i['FPARAM'][i1,1],'ro')
     # color code by [M/H]
-    #plots.plotc(ax1,l30i['FPARAM'][i1,0],l30i['FPARAM'][i1,1]-apokasc[logg][i2],l30i['FPARAM'][i1,3],zr=[-1,0.5],xr=[3500,5000],yr=[-1,1],xt='Teff',yt='ASPCAP-Kepler log g',label=[3600,0.7,'l30i (MARCS)'],colorbar=True,zt='[M/H]')
+    #plots.plotc(ax1,l30i['FPARAM'][i1,0],l30i['FPARAM'][i1,1]-apokasc[logg][i2],l30i['FPARAM'][i1,3],zr=[-1,0.5],xr=[3500,5000],yr=[-1,1],xt='Teff',yt='ASPCAP-seismic log g',label=[3600,0.7,'l30i (MARCS)'],colorbar=True,zt='[M/H]')
     # color code by [alpha/M]
-    plots.plotc(ax1,l30i['FPARAM'][i1,0],l30i['FPARAM'][i1,1]-apokasc[logg][i2],l30i['FPARAM'][i1,6],zr=[-0.1,0.4],xr=[3500,5000],yr=[-1,1],xt='Kepler log g',yt='ASPCAP-Kepler log g',label=[3600,0.7,'l30i (MARCS)'],colorbar=True,zt='[alpha/M]')
+    plots.plotc(ax1,l30i['FPARAM'][i1,0],l30i['FPARAM'][i1,1]-apokasc[logg][i2],l30i['FPARAM'][i1,6],zr=[-0.1,0.4],xr=[3500,5000],yr=[-1,1],xt='seismic log g',yt='ASPCAP-seismic log g',label=[3600,0.7,'l30i (MARCS)'],colorbar=True,zt='[alpha/M]')
     # plot ATMOS_HOLE_WARN and BAD points 
     plots.plotp(ax1,l30i['FPARAM'][i1[warn],0],l30i['FPARAM'][i1[warn],1]-apokasc[logg][i2[warn]],color='y')
     plots.plotp(ax1,l30i['FPARAM'][i1[bad],0],l30i['FPARAM'][i1[bad],1]-apokasc[logg][i2[bad]],color='r')
     # colod code by Teff
-    #plots.plotc(ax2,l30i['FPARAM'][i1,1],l30i['FPARAM'][i1,1]-apokasc[logg][i2],l30i['FPARAM'][i1,0],zr=[3500,5000],xr=[0,5],yr=[-1,1],xt='Kepler log g',yt='ASPCAP-Kepler log g',label=[4,0.7,'l30i'],colorbar=True,zt='Teff')
+    #plots.plotc(ax2,l30i['FPARAM'][i1,1],l30i['FPARAM'][i1,1]-apokasc[logg][i2],l30i['FPARAM'][i1,0],zr=[3500,5000],xr=[0,5],yr=[-1,1],xt='seismic log g',yt='ASPCAP-seismic log g',label=[4,0.7,'l30i'],colorbar=True,zt='Teff')
 
     # match dr13 with APOKASC
     i1,i2=match.match(dr13['APOGEE_ID'],apokasc['2MASS_ID'])
     #plt.plot(apokasc[logg][i2],dr13['FPARAM'][i1,1],'bo')
     #plt.xlim(1.60,1.15)
     #plt.ylim(1.15,1.85)
-    #plots.plotc(ax2,dr13['FPARAM'][i1,0],dr13['FPARAM'][i1,1]-apokasc[logg][i2],dr13['FPARAM'][i1,3],zr=[-1,0.5],xr=[3500,5000],yr=[-1,1],xt='Teff',yt='ASPCAP-Kepler log g',label=[3600,0.7,'dr13 (Kurucz)'],colorbar=True,zt='[M/H]')
-    plots.plotc(ax2,dr13['FPARAM'][i1,0],dr13['FPARAM'][i1,1]-apokasc[logg][i2],dr13['FPARAM'][i1,6],zr=[-0.1,0.4],xr=[3500,5000],yr=[-1,1],xt='Kepler log g',yt='ASPCAP-Kepler log g',label=[3600,0.7,'dr13 (Kurucz)'],colorbar=True,zt='[alpha/M]')
-    ##plots.plotc(ax4,dr13['FPARAM'][i1,1],dr13['FPARAM'][i1,1]-apokasc[logg][i2],dr13['FPARAM'][i1,0],zr=[3500,5000],xr=[0,5],yr=[-1,1],xt='Kepler log g',yt='ASPCAP-Kepler log g',label=[4,0.7,'dr13'],colorbar=True,zt='Teff')
+    #plots.plotc(ax2,dr13['FPARAM'][i1,0],dr13['FPARAM'][i1,1]-apokasc[logg][i2],dr13['FPARAM'][i1,3],zr=[-1,0.5],xr=[3500,5000],yr=[-1,1],xt='Teff',yt='ASPCAP-seismic log g',label=[3600,0.7,'dr13 (Kurucz)'],colorbar=True,zt='[M/H]')
+    plots.plotc(ax2,dr13['FPARAM'][i1,0],dr13['FPARAM'][i1,1]-apokasc[logg][i2],dr13['FPARAM'][i1,6],zr=[-0.1,0.4],xr=[3500,5000],yr=[-1,1],xt='seismic log g',yt='ASPCAP-seismic log g',label=[3600,0.7,'dr13 (Kurucz)'],colorbar=True,zt='[alpha/M]')
+    ##plots.plotc(ax4,dr13['FPARAM'][i1,1],dr13['FPARAM'][i1,1]-apokasc[logg][i2],dr13['FPARAM'][i1,0],zr=[3500,5000],xr=[0,5],yr=[-1,1],xt='seismic log g',yt='ASPCAP-seismic log g',label=[4,0.7,'dr13'],colorbar=True,zt='Teff')
 
     plt.tight_layout()
     plt.show()
@@ -74,33 +75,27 @@ def dr13(logg='LOGG_SYD_SCALING',apokasc='APOKASC_cat_v3.6.0.fits') :
     dr13=apload.allStar()[1].data
     apokasc=fits.open(apokasc)[1].data
     i1,i2=match.match(dr13['APOGEE_ID'],apokasc['2MASS_ID'])
-    rgb=np.where(apokasc['CONS_EVSTATES'][i2] == 'RGB'))[0]
-    rc=np.where(apokasc['CONS_EVSTATES'][i2] == 'RC'))[0]
+    rgb=np.where(apokasc['CONS_EVSTATES'][i2] == 'RGB')[0]
+    rc=np.where(apokasc['CONS_EVSTATES'][i2] == 'RC')[0]
 
-    fig=plt.figure()
+    fig,ax=plots.multi(2,3,hspace=0.5,wspace=0.001)
     # diff color-coded by gravity as f([M/H])
-    ax=fig.add_subplot(321)
-    plots.plotc(ax2,dr13['FPARAM'][i1,3],dr13['FPARAM'][i1,1]-apokasc[logg][i2],dr13['FPARAM'][i1,1],zr=[0,5],xr=[-2.5,0.5],yr=[-1,1],xt='[M/H]',yt='ASPCAP-Kepler log g',colorbar=True,zt='log g')
-    ax=fig.add_subplot(322)
-    plots.plotc(ax2,dr13['PARAM'][i1,3],dr13['PARAM'][i1,1]-apokasc[logg][i2],dr13['PARAM'][i1,1],zr=[0,5],xr=[-2.5,0.5],yr=[-1,1],xt='[M/H]',yt='ASPCAP-Kepler log g',colorbar=True,zt='log g')
+    plots.plotc(ax[0,0],dr13['FPARAM'][i1,3],dr13['FPARAM'][i1,1]-apokasc[logg][i2],dr13['FPARAM'][i1,1],zr=[0,5],xr=[-2.5,0.5],yr=[-1,1],xt='[M/H]',yt='ASPCAP-seismic log g')
+    plots.plotc(ax[0,1],dr13['PARAM'][i1,3],dr13['PARAM'][i1,1]-apokasc[logg][i2],dr13['PARAM'][i1,1],zr=[0,5],xr=[-2.5,0.5],yr=[-1,1],xt='[M/H]',colorbar=True,zt='log g')
 
     # diff color-coded by [M/H] as f(log g)
-    ax=fig.add_subplot(323)
-    plots.plotc(ax2,dr13['FPARAM'][i1,1],dr13['FPARAM'][i1,1]-apokasc[logg][i2],dr13['FPARAM'][i1,3],xr=[0,5],zr=[-2.5,0.5],yr=[-1,1],zt='[M/H]',yt='ASPCAP-Kepler log g',colorbar=True,xt='log g')
-    ax=fig.add_subplot(324)
-    plots.plotc(ax2,dr13['PARAM'][i1,1],dr13['PARAM'][i1,1]-apokasc[logg][i2],dr13['PARAM'][i1,3],xr=[0,5],zr=[-2.5,0.5],yr=[-1,1],zt='[M/H]',yt='ASPCAP-Kepler log g',colorbar=True,xt='log g')
+    plots.plotc(ax[1,0],dr13['FPARAM'][i1,1],dr13['FPARAM'][i1,1]-apokasc[logg][i2],dr13['FPARAM'][i1,3],xr=[0,5],zr=[-2.5,0.5],yr=[-1,1],zt='[M/H]',yt='ASPCAP-seismic log g',xt='log g')
+    plots.plotc(ax[1,1],dr13['PARAM'][i1,1],dr13['PARAM'][i1,1]-apokasc[logg][i2],dr13['PARAM'][i1,3],xr=[0,5],zr=[-2.5,0.5],yr=[-1,1],zt='[M/H]',colorbar=True,xt='log g')
 
     # RGB and RC a f(log g)
-    ax=fig.add_subplot(325)
-    plots.plotp(ax2,dr13['FPARAM'][i1[rgb],1],dr13['FPARAM'][i1[rgb],1]-apokasc[logg][i2[rgb]],xr=[0,5,yr=[-1,1],xt='Kepler log g',yt='ASPCAP-Kepler log g',label=[3600,0.7,'dr13 (Kurucz)'],colorbar=True,color='r')
-    plots.plotp(ax2,dr13['FPARAM'][i1[rc],1],dr13['FPARAM'][i1[rc],1]-apokasc[logg][i2[rc]],xr=[0,5],yr=[-1,1],xt='Kepler log g',yt='ASPCAP-Kepler log g',label=[3600,0.7,'dr13 (Kurucz)'],colorbar=True,color='b')
-    ax=fig.add_subplot(326)
-    plots.plotp(ax2,dr13['PARAM'][i1[rgb],1],dr13['PARAM'][i1[rgb],1]-apokasc[logg][i2[rgb]],xr=[0,5],yr=[-1,1],xt='log g',yt='ASPCAP-Kepler log g',label=[3600,0.7,'dr13 (Kurucz)'],colorbar=True,color='r')
-    plots.plotp(ax2,dr13['PARAM'][i1[rc],1],dr13['PARAM'][i1[rc],1]-apokasc[logg][i2[rc]],xr=[0,5],yr=[-1,1],xt='log g',yt='ASPCAP-Kepler log g',label=[3600,0.7,'dr13 (Kurucz)'],colorbar=True,color='b')
+    plots.plotp(ax[2,0],dr13['FPARAM'][i1[rgb],1],dr13['FPARAM'][i1[rgb],1]-apokasc[logg][i2[rgb]],xr=[0,5],yr=[-1,1],xt='seismic log g',yt='ASPCAP-seismic log g',label=[3600,0.7,'dr13 (Kurucz)'],color='r')
+    plots.plotp(ax[2,0],dr13['FPARAM'][i1[rc],1],dr13['FPARAM'][i1[rc],1]-apokasc[logg][i2[rc]],xr=[0,5],yr=[-1,1],xt='seismic log g',yt='ASPCAP-seismic log g',label=[3600,0.7,'dr13 (Kurucz)'],color='b')
+    plots.plotp(ax[2,1],dr13['PARAM'][i1[rgb],1],dr13['PARAM'][i1[rgb],1]-apokasc[logg][i2[rgb]],xr=[0,5],yr=[-1,1],xt='log g',label=[3600,0.7,'dr13 (Kurucz)'],color='r')
+    plots.plotp(ax[2,1],dr13['PARAM'][i1[rc],1],dr13['PARAM'][i1[rc],1]-apokasc[logg][i2[rc]],xr=[0,5],yr=[-1,1],xt='log g',label=[3600,0.7,'dr13 (Kurucz)'],color='b')
     
 
 
-def dr13dr12i() :
+def dr13dr12() :
     '''
     ASPCAP compared with physical and asteroseismic log g, DR13/DR12/l30i
     '''
@@ -137,7 +132,7 @@ def dr13dr12i() :
         j=apselect.clustmember(dr13,cluster,raw=True)
         lum=10.**(-0.4*(dr13['H'][j]+isochrones.bc(dr13['FPARAM'][j,0],filt='h',agerange=[10,10.1])-(5*np.log10(dist)-5)-4.74))*astropy.constants.L_sun.cgs.value
         logg=np.log10(4*np.pi*astropy.constants.G.cgs.value*mass*astropy.constants.M_sun.cgs.value*astropy.constants.sigma_sb.cgs.value*dr13['FPARAM'][j,0]**4/lum)
-        plots.plotc(ax[0,1[,dr13['FPARAM'][j,3]*0+mh,dr13['FPARAM'][j,1]-logg,dr13['FPARAM'][j,0],xr=[-2.75,-1.],yr=[-2,2],zr=[3500,5500],label=[-2.5,1.5,'DR13 raw'])
+        plots.plotc(ax[0,1],dr13['FPARAM'][j,3]*0+mh,dr13['FPARAM'][j,1]-logg,dr13['FPARAM'][j,0],xr=[-2.75,-1.],yr=[-2,2],zr=[3500,5500],label=[-2.5,1.5,'DR13 raw'])
         plots.plotc(ax[1,1],dr13['PARAM'][j,3]*0+mh,dr13['PARAM'][j,1]-logg,dr13['PARAM'][j,0],xr=[-2.75,-1.],yr=[-2,2],zr=[3500,5500],label=[-2.5,1.5,'DR13 cal'],xt='[M/H]')
 
         #l30i
@@ -155,7 +150,7 @@ def dr13dr12i() :
     
     i1,i2=match.match(dr13['APOGEE_ID'],apokasc['2MASS_ID'])
     plots.plotc(ax[0,0],dr13['FPARAM'][i1,3],dr13['FPARAM'][i1,1]-apokasc['LOGG_SYD_SCALING'][i2],dr13['FPARAM'][i1,0],xr=[-2.5,1.],yr=[-2,2],zr=[3500,5500],xt='[M/H]',label=[-2.,1.5,'DR13 raw'])
-    plots.plotc(ax[1,0],dr13['PARAM'][i1,3],dr13['PARAM'][i1,1]-apokasc['LOGG_SYD_SCALING'][i2],dr13['PARAM'][i1,0],xr=[-2.5,1.],yr=[-2,2],zr=[3500,5500],xt='[M/H]',yt='ASPCAP-Kepler log g',label=[-2.,1.5,'DR13 cal'])
+    plots.plotc(ax[1,0],dr13['PARAM'][i1,3],dr13['PARAM'][i1,1]-apokasc['LOGG_SYD_SCALING'][i2],dr13['PARAM'][i1,0],xr=[-2.5,1.],yr=[-2,2],zr=[3500,5500],xt='[M/H]',yt='ASPCAP-seismic log g',label=[-2.,1.5,'DR13 cal'])
 
     i1,i2=match.match(dr12['APOGEE_ID'],apokasc['2MASS_ID'])
     plots.plotc(ax[0,1],dr12['FPARAM'][i1,3],dr12['FPARAM'][i1,1]-apokasc['LOGG_SYD_SCALING'][i2],dr12['FPARAM'][i1,0],xr=[-2.5,1.],yr=[-2,2],zr=[3500,5500],label=[-2,1.5,'DR12 raw'])
--- ref.py
+++ ref.py
@@ -4,8 +4,8 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 from sdss.apogee import apload
-from holtz import match
-from holtz import plots
+from holtz.tools import match
+from holtz.tools import plots
 from astropy.io import fits
 import pdb
 
@@ -39,6 +39,7 @@ def plot(ax,a,r,y,xaxis='TEFF',zaxis='M_H',types=None,size=[40],xr=[3500,6000],z
 
 
 def main():
+    apload.dr13()
     a=apload.allStar()[1].data
     m1,ref,m2 = get_ref(a)
     a=a[m1]
@@ -49,64 +50,92 @@ def main():
 
     # Teff
     n=2
-    fig,ax=plt.subplots(n,figsize=(8,2+n))
-    fig.subplots_adjust(hspace=0.001,wspace=0.001)
+    fig,ax=plots.multi(1,n,figsize=(8,2+n),hspace=0.001)
+    #fig,ax=plt.subplots(n,figsize=(8,2+n))
+    #fig.subplots_adjust(hspace=0.001,wspace=0.001)
     #plot(ax[0],a,ref,a['TEFF']-ref['TEFF'],types=types,size=size,marker=marker,yr=[-490,490],yt='Teff')
     #plot(ax[1],a,ref,a['LOGG']-ref['LOGG'],types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='log g')
-    plots.plotp(ax[0],a['TEFF'],a['TEFF']-ref['TEFF'],ref['class'],xt='Teff',yt='$\Delta T_{eff}$,xr=[3500,6000],yr=[-490,490],
-                types=types,size=size,marker=marker)
-    plots.plotp(ax[0],a['TEFF'],a['LOGG']-ref['LOGG'],ref['class'],xt='Teff',yt='$\Delta log g$,xr=[3500,6000],yr=[-0.9,0.9],
-                types=types,size=size,marker=marker)
-    ticklabels = ax[0].get_xticklabels() 
-    plt.setp(ticklabels, visible=False)
+
+    plots.plotp(ax[0],a['TEFF'],a['TEFF']-ref['TEFF'],z=a['M_H'],xt='Teff',yt='$\Delta T_{eff}$',xr=[3500,6000],yr=[-490,490],zr=[-2,0.5],
+                typeref=ref['class'],types=types,size=size,marker=marker)
+    plots.plotp(ax[1],a['TEFF'],a['LOGG']-ref['LOGG'],z=a['M_H'],xt='Teff',yt='$\Delta log g$',xr=[3500,6000],yr=[-0.9,0.9],zr=[-2,0.5],
+                typeref=ref['class'],types=types,size=size,marker=marker)
+    #ticklabels = ax[0].get_xticklabels() 
+    #plt.setp(ticklabels, visible=False)
     pdb.set_trace()
 
     # CNO
     n=3
-    fig,ax=plt.subplots(n,figsize=(8,2+n))
-    fig.subplots_adjust(hspace=0.001,wspace=0.001)
+    fig,ax=plots.multi(1,n,figsize=(8,2+n),hspace=0.001)
+    plots.plotp(ax[0],a['TEFF'],a['C_FE']-(ref['C_H']-ref['FE_H']),z=a['M_H'],typeref=ref['class'],
+                xt='Teff',yt='$\Delta [C/Fe]$',xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
+    plots.plotp(ax[1],a['TEFF'],a['N_FE']-(ref['N_H']-ref['FE_H']),z=a['M_H'],typeref=ref['class'],
+                xt='Teff',yt='$\Delta [N/Fe]$',xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
+    plots.plotp(ax[2],a['TEFF'],a['O_FE']-(ref['O_H']-ref['FE_H']),z=a['M_H'],typeref=ref['class'],
+                xt='Teff',yt='$\Delta [O/Fe]$',xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
+    #fig.subplots_adjust(hspace=0.001,wspace=0.001)
     #plot(ax[0],a,ref,a['C_FE']-(ref['C_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[C/Fe]')
     #plot(ax[1],a,ref,a['N_FE']-(ref['N_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[N/Fe]')
     #plot(ax[2],a,ref,a['O_FE']-(ref['O_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[O/Fe]')
-    plots.plotp(ax[0],a['TEFF'],a['C_FE']-(ref['C_H']-ref['FE_H']),ref['class'],
-                xt='Teff',yt='$\Delta [C/Fe]$,xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
-    plots.plotp(ax[1],a['TEFF'],a['N_FE']-(ref['N_H']-ref['FE_H']),ref['class'],
-                xt='Teff',yt='$\Delta [C/Fe]$,xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
-    plots.plotp(ax[2],a['TEFF'],a['O_FE']-(ref['O_H']-ref['FE_H']),ref['class'],
-                xt='Teff',yt='$\Delta [C/Fe]$,xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
-    ticklabels = ax[0].get_xticklabels()
-    for i in range(1,n-1) : ticklabels = ticklabels + ax[i].get_xticklabels() 
-    plt.setp(ticklabels, visible=False)
+    #ticklabels = ax[0].get_xticklabels()
+    #for i in range(1,n-1) : ticklabels = ticklabels + ax[i].get_xticklabels() 
+    #plt.setp(ticklabels, visible=False)
     pdb.set_trace()
 
     # alpha
     n=5
-    fig,ax=plt.subplots(n,figsize=(8,2+n))
-    fig.subplots_adjust(hspace=0.001,wspace=0.001)
-    plot(ax[0],a,ref,a['MG_FE']-(ref['MG_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Mg/Fe]')
-    plot(ax[1],a,ref,a['SI_FE']-(ref['SI_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Si/Fe]')
-    plot(ax[2],a,ref,a['S_FE']-(ref['S_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[S/Fe]')
-    plot(ax[3],a,ref,a['CA_FE']-(ref['CA_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Ca/Fe]')
-    plot(ax[4],a,ref,a['TI_FE']-(ref['TI_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Ti/Fe]')
-    ticklabels = ax[0].get_xticklabels()
-    for i in range(1,n-1) : ticklabels = ticklabels + ax[i].get_xticklabels() 
-    plt.setp(ticklabels, visible=False)
+    fig,ax=plots.multi(1,n,figsize=(8,2+n),hspace=0.001)
+    plots.plotp(ax[0],a['TEFF'],a['MG_FE']-(ref['MG_H']-ref['FE_H']),z=a['M_H'],typeref=ref['class'],
+                xt='Teff',yt='$\Delta [Mg/Fe]$',xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
+    plots.plotp(ax[1],a['TEFF'],a['SI_FE']-(ref['SI_H']-ref['FE_H']),z=a['M_H'],typeref=ref['class'],
+                xt='Teff',yt='$\Delta [Si/Fe]$',xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
+    plots.plotp(ax[2],a['TEFF'],a['S_FE']-(ref['S_H']-ref['FE_H']),z=a['M_H'],typeref=ref['class'],
+                xt='Teff',yt='$\Delta [S/Fe]$',xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
+    plots.plotp(ax[3],a['TEFF'],a['CA_FE']-(ref['CA_H']-ref['FE_H']),z=a['M_H'],typeref=ref['class'],
+                xt='Teff',yt='$\Delta [Ca/Fe]$',xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
+    plots.plotp(ax[4],a['TEFF'],a['TI_FE']-(ref['TI_H']-ref['FE_H']),z=a['M_H'],typeref=ref['class'],
+                xt='Teff',yt='$\Delta [Ti/Fe]$',xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
+    #fig,ax=plt.subplots(n,figsize=(8,2+n))
+    #fig.subplots_adjust(hspace=0.001,wspace=0.001)
+    #plot(ax[0],a,ref,a['MG_FE']-(ref['MG_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Mg/Fe]')
+    #plot(ax[1],a,ref,a['SI_FE']-(ref['SI_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Si/Fe]')
+    #plot(ax[2],a,ref,a['S_FE']-(ref['S_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[S/Fe]')
+    #plot(ax[3],a,ref,a['CA_FE']-(ref['CA_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Ca/Fe]')
+    #plot(ax[4],a,ref,a['TI_FE']-(ref['TI_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Ti/Fe]')
+    #ticklabels = ax[0].get_xticklabels()
+    #for i in range(1,n-1) : ticklabels = ticklabels + ax[i].get_xticklabels() 
+    #plt.setp(ticklabels, visible=False)
     pdb.set_trace()
 
     # others
     n=7
-    fig,ax=plt.subplots(n,figsize=(8,2+n))
-    fig.subplots_adjust(hspace=0.001,wspace=0.001)
-    plot(ax[0],a,ref,a['NA_FE']-(ref['NA_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Na/Fe]')
-    plot(ax[1],a,ref,a['AL_FE']-(ref['AL_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Al/Fe]')
-    plot(ax[2],a,ref,a['K_FE']-(ref['K_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[K/Fe]')
-    plot(ax[3],a,ref,a['V_FE']-(ref['V_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[V/Fe]')
-    plot(ax[4],a,ref,a['MN_FE']-(ref['MN_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Mn/Fe]')
-    plot(ax[5],a,ref,a['FE_H']-ref['FE_H'],types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Fe/H]')
-    plot(ax[6],a,ref,a['NI_FE']-(ref['NI_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Ni/Fe]')
-    ticklabels = ax[0].get_xticklabels()
-    for i in range(1,n-1) : ticklabels = ticklabels + ax[i].get_xticklabels() 
-    plt.setp(ticklabels, visible=False)
+    fig,ax=plots.multi(1,n,figsize=(8,2+n),hspace=0.001)
+    plots.plotp(ax[0],a['TEFF'],a['NA_FE']-(ref['NA_H']-ref['FE_H']),z=a['M_H'],typeref=ref['class'],
+                xt='Teff',yt='$\Delta [Na/Fe]$',xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
+    plots.plotp(ax[1],a['TEFF'],a['AL_FE']-(ref['AL_H']-ref['FE_H']),z=a['M_H'],typeref=ref['class'],
+                xt='Teff',yt='$\Delta [AL/Fe]$',xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
+    plots.plotp(ax[2],a['TEFF'],a['K_FE']-(ref['K_H']-ref['FE_H']),z=a['M_H'],typeref=ref['class'],
+                xt='Teff',yt='$\Delta [K/Fe]$',xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
+    plots.plotp(ax[3],a['TEFF'],a['V_FE']-(ref['V_H']-ref['FE_H']),z=a['M_H'],typeref=ref['class'],
+                xt='Teff',yt='$\Delta [V/Fe]$',xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
+    plots.plotp(ax[4],a['TEFF'],a['MN_FE']-(ref['MN_H']-ref['FE_H']),z=a['M_H'],typeref=ref['class'],
+                xt='Teff',yt='$\Delta [Mn/Fe]$',xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
+    plots.plotp(ax[5],a['TEFF'],a['FE_H']-ref['FE_H'],z=a['M_H'],typeref=ref['class'],
+                xt='Teff',yt='$\Delta [Fe/H]$',xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
+    plots.plotp(ax[6],a['TEFF'],a['NI_FE']-(ref['NI_H']-ref['FE_H']),z=a['M_H'],typeref=ref['class'],
+                xt='Teff',yt='$\Delta [Ni/Fe]$',xr=[3500,6000],yr=[-0.9,0.9],types=types,size=size,marker=marker)
+    #fig,ax=plt.subplots(n,figsize=(8,2+n))
+    #fig.subplots_adjust(hspace=0.001,wspace=0.001)
+    #plot(ax[0],a,ref,a['NA_FE']-(ref['NA_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Na/Fe]')
+    #plot(ax[1],a,ref,a['AL_FE']-(ref['AL_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Al/Fe]')
+    #plot(ax[2],a,ref,a['K_FE']-(ref['K_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[K/Fe]')
+    #plot(ax[3],a,ref,a['V_FE']-(ref['V_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[V/Fe]')
+    #plot(ax[4],a,ref,a['MN_FE']-(ref['MN_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Mn/Fe]')
+    #plot(ax[5],a,ref,a['FE_H']-ref['FE_H'],types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Fe/H]')
+    #plot(ax[6],a,ref,a['NI_FE']-(ref['NI_H']-ref['FE_H']),types=types,size=size,marker=marker,yr=[-0.9,0.9],yt='[Ni/Fe]')
+    #ticklabels = ax[0].get_xticklabels()
+    #for i in range(1,n-1) : ticklabels = ticklabels + ax[i].get_xticklabels() 
+    #plt.setp(ticklabels, visible=False)
     pdb.set_trace()
 
 
