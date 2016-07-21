7c804753dd10b754add2c0f78bff7a0b4915765a
Jon Holtzman <holtz@antares.nmsu.edu>
2016-07-21 06:43:17 -0600

    commit

--- teffcomp.py
+++ teffcomp.py
@@ -22,11 +22,11 @@ def dr13() :
     dr13=apload.allStar()[1].data
     irfm=fits.open(os.environ['IDLWRAP_DIR']+'/data/irfm_temp.fits')[1].data
     i1,i2=match.match(dr13['APOGEE_ID'],irfm['2MASS ID'])
+    pdb.set_trace()
 
     fig,ax=plots.multi(2,3,hspace=0.5,wspace=0.001)
     # diff color-coded by gravity as f([M/H])
     plots.plotc(ax[0,0],dr13['FPARAM'][i1,3],dr13['FPARAM'][i1,0]-irfm['IRFM TEFF'][i2],dr13['FPARAM'][i1,1],zr=[0,5],xr=[-2.5,0.5],yr=[-300,300],xt='[M/H]',yt='ASPCAP-seismic Teff')
-    pdb.set_trace()
     plots.plotc(ax[0,1],dr13['PARAM'][i1,3],dr13['PARAM'][i1,0]-irfm['IRFM TEFF'][i2],dr13['PARAM'][i1,1],zr=[0,5],xr=[-2.5,0.5],yr=[-300,300],xt='[M/H]',colorbar=True,zt='Teff')
 
     # diff color-coded by [M/H] as f(log g)
