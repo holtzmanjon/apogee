70d318cc2b2162148f4018c6e575d06927a719f5
Jon Holtzman <holtz@nmsu.edu>
2016-09-06 06:21:27 -0600

    start to outline routines

--- aspcap.py
+++ aspcap.py
@@ -7,3 +7,26 @@ def params() :
     params=['TEFF','LOGG','LOG10VDOP','METALS','C','N','O Mg Si S Ca Ti','LGVSINI','O']
     return params,tagnames,flagnames
 
+def readstars(starlist,libpar) :
+    '''
+    Runs stars in starlist through FERRE using libpar
+    '''
+    for star in starlist :
+        spec,err=readstar(star)
+        cont=cont_normalize(spec)
+
+def ferre(spec,libpar,te0=None,logg0=None,mh0=None) :
+    '''
+    Runs FERRE for a set of input spectra
+    '''
+    np.savetxt(out+'.frd',spec)
+    np.savetxt(out+'.err',spec)
+    np.savetxt(out+'.con',spec)
+
+    # write ferre control file
+    ferre.writeferre()
+
+    out=ferre.readferre(name)
+
+       
+
